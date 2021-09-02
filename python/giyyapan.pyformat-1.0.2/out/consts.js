1bb7e3cb91c5e46015030ca35dbf56680b2cbeb0
David Montaño <77401753+ndavidmb@users.noreply.github.com>
2021-09-02 08:55:12 -0500

    Delete python directory

--- python/giyyapan.pyformat-1.0.2/out/consts.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.IDENTIFIER = 'pyformat';
-exports.PYTHON_LANGUAGE = 'python';
-exports.PYTHON = [
-    { scheme: 'file', language: exports.PYTHON_LANGUAGE },
-    { scheme: 'untitled', language: exports.PYTHON_LANGUAGE }
-];
-//# sourceMappingURL=consts.js.map
